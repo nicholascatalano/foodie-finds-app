db96e93c57e94a06d89bc652ba63237eebca6a11
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-11 14:00:12 -0500

    adds relationships to user and review models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,17 @@
 //import models
-const User = require("./User");
+const User = require('./User');
+const Review = require('./Review');
+
+User.hasMany(Review, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Review.belongsTo(User, {
+  foreignKey: 'user_id',
+});
 
 module.exports = {
   User,
+  Review,
 };
