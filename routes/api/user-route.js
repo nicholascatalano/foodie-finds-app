f9a0c132d9601a6568e7edf33a1b7414cc40af93
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-11 11:55:58 -0700

    added a login route

--- routes/api/user-route.js
+++ routes/api/user-route.js
@@ -1,12 +1,25 @@
-const router = require("express").Router();
-const {User} = require("../../models");
+const router = require('express').Router();
+const { User } = require('../../models');
 
+// api/users endpoint
 
-// api/users endpoint 
+router.post('/login', async (req, res) => {
+  //find the user with the username
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
 
-//Create a login
-router.post('/login', async (req,res)=>{
-//find the user 
-//create a session
+    //create a session
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'You are logged in!' });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 module.exports = router;
