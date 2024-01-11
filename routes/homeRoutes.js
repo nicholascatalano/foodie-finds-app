43255695d0c18a14d604b21279430c2d0aa3e550
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-11 12:02:20 -0700

    added /login route in the homeRoutes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,3 +1,15 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const { User } = require('../models');
+
+//login page
+router.get('/login', (req, res) => {
+  //if a session exists - redirect to the homepage
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  // render login page
+  res.render('login');
+});
 
 module.exports = router;
