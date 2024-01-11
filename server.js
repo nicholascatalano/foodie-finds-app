417790fc5fac9f8e5723179211718ab7367b31ad
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-11 12:18:13 -0500

    informs express.js on which template engine to use

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ const sess = {
 
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
