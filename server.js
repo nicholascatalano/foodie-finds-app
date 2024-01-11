06b0629c7e59c56479ed3c9030e8adef3b7ed0af
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-11 12:05:50 -0500

    adds app.listen to start server

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const routes = require("./controllers");
-const helpers = require("./utils/helpers");
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
