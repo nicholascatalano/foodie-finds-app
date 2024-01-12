960e63a8810967fc7ca7bae3cc105221faa3effb
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-12 13:21:05 -0500

    adds /review api route in api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user-route');
+const reviewRoutes = require('/review-route');
 
 router.use('/users', userRoutes);
+
+router.use('/review', reviewRoutes);
 
 module.exports = router;
