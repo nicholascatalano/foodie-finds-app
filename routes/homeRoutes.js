2b4cbf6257c92bdc15dd6212b1de3d62caa54284
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-12 14:46:40 -0500

    changed logged_in to loggedIn to match our session object

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Review } = require('../models');
-const withAuth = require('../../utils/auth');
+const withAuth = require('../utils/auth');
 
 // GET all reviews for homepage
 router.get('/', withAuth, async (req, res) => {
