8bea75eb0e0c7e92994d55ae8842f1ec7b551ed1
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-12 10:44:46 -0500

    adds post route in api/users to create new User session

--- routes/api/user-route.js
+++ routes/api/user-route.js
@@ -3,6 +3,24 @@ const { User } = require('../../models');
 
 // api/users endpoint
 
+// route to create new User session
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      (req.session.userId = userData.id),
+        (req.session.username = userData.username),
+        (req.session.loggedIn = true),
+        res.json(userData);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
   //find the user with the username
   try {
