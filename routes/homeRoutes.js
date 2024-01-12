d2b8608325d117d707c91e95ac279a02a74f3b7a
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-12 11:17:27 -0500

    made changes to signup and login handlebars

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Review } = require('../models');
 
 // GET all reviews for homepage
 router.get('/', async (req, res) => {
