153179bf5fbce1690a7e93a5f3c9b22cb7689dcf
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-12 10:40:47 -0500

    adds temporary res.render to homepage get route until we have review Data to display

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,6 +1,23 @@
 const router = require('express').Router();
 const { User } = require('../models');
 
+// GET all reviews for homepage
+router.get('/', async (req, res) => {
+  // try {
+  //   const reviewData = await Review.findAll({
+  //     include: [User],
+  //   });
+  //   const reviews = reviewData.map((review) => review.get({ plain: true }));
+  //   // render on allPosts view
+  //   res.render('homepage', {
+  //     reviews,
+  //   });
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
+  res.render('homepage')
+});
+
 //login page
 router.get('/login', (req, res) => {
   //if a session exists - redirect to the homepage
