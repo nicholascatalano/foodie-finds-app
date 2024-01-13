4f1018b0657d2b945443f59377c171f123922cbc
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-12 19:26:06 -0700

    created two fetch requests using the TripAdvisor content API, one to get the locationID and the other one to get the details using that locationID

---
+++ public/js/searchRestaurant.js
@@ -0,0 +1,31 @@
+// the user inputs the name of the restaurant
+// option to add the city as well
+async function fetchRestaurantDate() {
+  const queryForm = document.querySelector('#review-restaurant').value.trim(); //from the new review form
+  const cityForm = document.querySelector('#review-city').value.trim(); //from review form
+
+  const apiKey = 'C0F90B543BB44183916FEE462F90E8DD';
+  const searchQuery = queryForm.split(' ').join('%20') + '%20' + cityForm;
+
+  //this will look for the restaurant location using Tripadvisor content API
+  const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+  const response = await fetch(
+    `https://api.content.tripadvisor.com/api/v1/location/search?searchQuery=${searchQuery}&category=restaurants&language=en&key=${apiKey}`,
+    options
+  );
+
+  //response object will have data array - assume the correct hit will be the first
+  const restaurantData = response.data[0];
+  const locationId = restaurantData.location_id; //this will be used to get the restaurant details with a second API search
+
+  const restaurantDetails = await fetch(
+    `https://api.content.tripadvisor.com/api/v1/location/${locationId}/details?language=en&currency=USD&key=${apiKey}`,
+    options
+  );
+
+//   restaurantDetails will have name, address_obj.city, cuisine, website, price_level, subcategories
+//      can make a post request to create a restaurant so we can render it 
+}
+
+fetchRestaurantDate();
