602eeb87e2e185b54f60b2d9e08fc227029eda1f
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-13 12:49:20 -0700

    started route to create restaurant - waiting on model

--- routes/api/restaurantRoutes.js
+++ routes/api/restaurantRoutes.js
@@ -3,6 +3,17 @@ const { User, Review, Restaurant } = require('../../models');
 
 // endpoint -> api/restaurants
 
-
+//POST request to create a new restaurant from the review form
+router.post('/', async (req, res) => {
+  try {
+    const newRestaurant = await Restaurant.create({
+      // ...req.body
+      // waiting on the model
+    });
+    res.status(200).json(newRestaurant);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
