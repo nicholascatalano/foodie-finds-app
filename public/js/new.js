39dc583d50b6a8d4e0c3427ba9da9f51c27426f1
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-14 11:59:57 -0700

    added function to look up in the db for the restaurant in the new.js file

--- public/js/new.js
+++ public/js/new.js
@@ -5,21 +5,25 @@ const newPostHandler = async function (event) {
   const rating = document.querySelector('#review-rating').value;
   const content = document.querySelector('#review-content').value;
 
-  if (city && restaurant && rating && rating && content) {
-    await fetch('/api/review', {
-      method: 'POST',
-      body: JSON.stringify({
-        city,
-        restaurant,
-        rating,
-        content,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    document.location.replace('/');
-  }
+  const restaurant_id = await fetchRestaurant_id(restaurant, city);
+  console.log('restaurant_id', restaurant_id);
+
+  // if (city && restaurant && rating && rating && content) {
+  //   await fetch('/api/review', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       city,
+  //       restaurant,
+  //       rating,
+  //       content,
+  //       //needs restaurant_id
+  //     }),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
+  //   document.location.replace('/');
+  // }
 };
 
 document
--- public/js/searchRestaurant.js
+++ public/js/searchRestaurant.js
@@ -1,57 +1,63 @@
-//first check if the restaurant is in the db using the name and city
-const isRestaurantInDb = async (restaurantName, city) => {
-  //will look for the restaunt using this fetch request
+//fetchRestaurant_id will first look for the restaurant in the database
+//it will return the restaurant_id
+const fetchRestaurant_id = async (restaurantName, city) => {
+  //will look for the restaunt in the db using this fetch request
   const response = await fetch(`/api/restaurants/${restaurantName}/${city}`);
 
-  //if response.ok
+  //if response.ok it will return the existing restaurant_id 
   if (response.ok) {
-    //it already exists. assign the restaurant id ?? confused on what to do here
+    //it already exists. Grab the restaurant_id from the response
+    const restaurantInfo = await response.json();
+    let restaurant_id = restaurantInfo.id;
+    return restaurant_id;
+    //if not, it will create a new restaurant using the fetchRestaurantData function
   } else {
     //lookit up using the external api
-    fetchRestaurantData(restaurantName, city);
+    console.log(response);
+    return;
   }
 };
 
-//fetchRestaurantData will return true if a new restaurant was successfully created 
-async function fetchRestaurantData(restaurantName, city) {
-  const apiKey = process.env.DB_API_KEY;
-  //remove spaces and replace them with %20 to match the format for the search query
-  const searchQuery =
-    restaurantName.split(' ').join('%20') + '%20' + city.split(' ').join('%20');
+//fetchRestaurantData will return true if a new restaurant was successfully created
+// async function fetchRestaurantData(restaurantName, city) {
+//   const apiKey = process.env.DB_API_KEY;
+//   //remove spaces and replace them with %20 to match the format for the search query
+//   const searchQuery =
+//     restaurantName.split(' ').join('%20') + '%20' + city.split(' ').join('%20');
 
-  //this will look for the restaurant location using Tripadvisor content API
-  const options = { method: 'GET', headers: { accept: 'application/json' } };
+//   //this will look for the restaurant location using Tripadvisor content API
+//   const options = { method: 'GET', headers: { accept: 'application/json' } };
 
-  const response = await fetch(
-    `https://api.content.tripadvisor.com/api/v1/location/search?searchQuery=${searchQuery}&category=restaurants&language=en&key=${apiKey}`,
-    options
-  );
+//   const response = await fetch(
+//     `https://api.content.tripadvisor.com/api/v1/location/search?searchQuery=${searchQuery}&category=restaurants&language=en&key=${apiKey}`,
+//     options
+//   );
 
-  //response object will have data array - assume the correct hit will be the first
-  const locationId = response.data[0].location_id; //this will be used to get the restaurant details with a second API search
+//   //response object will have data array - assume the correct hit will be the first
+//   const locationId = response.data[0].location_id; //this will be used to get the restaurant details with a second API search
 
-  //get restaurant details using the locationId and save them in an object
-  const restaurantDetails = await fetch(
-    `https://api.content.tripadvisor.com/api/v1/location/${locationId}/details?language=en&currency=USD&key=${apiKey}`,
-    options
-  );
+//   //get restaurant details using the locationId and save them in an object
+//   const restaurantDetails = await fetch(
+//     `https://api.content.tripadvisor.com/api/v1/location/${locationId}/details?language=en&currency=USD&key=${apiKey}`,
+//     options
+//   );
 
-  //   deconstruct restaurantDetails with the info we need to send to the model
-  const {
-    location_id,
-    name,
-    address_obj: { city },
-    price_level,
-    cuisine,
-    subcategory,
-  } = restaurantDetails;
+//   //   deconstruct restaurantDetails with the info we need to send to the model
+//   const {
+//     location_id,
+//     name,
+//     address_obj: { city },
+//     price_level,
+//     cuisine,
+//     subcategory,
+//   } = restaurantDetails;
 
-  //   POST request for api/restaurants/ -> can make a post request to create a restaurant so we can render it
-  const newRest = await fetch('/api/restaurants/', {
-    mehod: 'POST',
-    body: JSON.stringify({ location_id, name, city, price_level }),
-    headers: { 'Content-Type': 'application/json' },
-  });
+//   //   POST request for api/restaurants/ -> can make a post request to create a restaurant so we can render it
+//   const newRest = await fetch('/api/restaurants/', {
+//     mehod: 'POST',
+//     body: JSON.stringify({ location_id, name, city, price_level }),
+//     headers: { 'Content-Type': 'application/json' },
+//   });
 
-  return newRest;
-}
+//   return newRest;
+// }
