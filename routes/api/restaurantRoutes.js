32e45f26175e17cd6fba0cc49686f0c8413a8591
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-13 19:21:31 -0700

    added req.body to the post request to create a restaurant

--- routes/api/restaurantRoutes.js
+++ routes/api/restaurantRoutes.js
@@ -22,8 +22,7 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const newRestaurant = await Restaurant.create({
-      // ...req.body
-      // waiting on the model
+      ...req.body,
     });
     res.status(200).json(newRestaurant);
   } catch (err) {
