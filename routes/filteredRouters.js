aae8a838c16857d5e165d6a2253d47b82654f818
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-15 15:48:22 -0700

    if the user selects multiple cuisine, it will look for all with the sequelize literal

--- routes/filteredRouters.js
+++ routes/filteredRouters.js
@@ -1,38 +1,29 @@
 const router = require('express').Router();
 const { Op } = require('sequelize');
-const sequelize = require('../../config/connection');
+const sequelize = require('../config/connection');
 const { Restaurant } = require('../models');
 
 //GET restaurants which match the applied filters by user -> end point api/restaurants/filter/restaurants/by
-router.get('/filter?', async (req, res) => {
+router.get('/?', async (req, res) => {
   try {
     //query can look with one or all
     //name=&cuisine=&price_range=&rating=&city=&type=
     console.log(req.query);
-    let filters;
+
     //building the sequelize literal to filter restaurants
-    // if (req.query.name && req.query.cuisine) {
-    //   filters = [
-    //     {
-    //       name: req.query.name,
-    //     },
-    //     {
-    //       cuisine: {
-    //         [Op.like]: sequelize.literal(`'%${req.query.cuisine}%'`),
-    //       },
-    //     },
-    //   ];
-    //   return filters;
-    // }
+    //expected will be cuisine=american,sushi,latin
+    const arrCuisine = req.query.cuisine.split(',');
+    let cuisineProperty = "'%" + arrCuisine[0] + "%'"; //let the first element of the array
+    for (i = 1; i < arrCuisine.length; i++) {
+      cuisineProperty += ' OR cuisine LIKE ' + "'%" + arrCuisine[i] + "%'";
+    }
+
     const filteredRestaurants = await Restaurant.findAll({
       where: {
         [Op.and]: [
           {
-            name: req.query.name,
-          },
-          {
             cuisine: {
-              [Op.like]: sequelize.literal(`'%${req.query.cuisine}%'`),
+              [Op.like]: sequelize.literal(`${cuisineProperty}`),
             },
           },
         ],
