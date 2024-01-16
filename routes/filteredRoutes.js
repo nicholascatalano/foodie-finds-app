279aa853483a01a5fc6ff590e80b74db0dd1ac31
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-16 12:21:48 -0700

    will filter by sub_categories

--- routes/filteredRoutes.js
+++ routes/filteredRoutes.js
@@ -10,8 +10,11 @@ router.get('/?', async (req, res) => {
     //query can look with one or all
     //name=&cuisine=&price_range=&rating=&city=&type=
     let cuisineProperty;
-    let filteredRestaurants;
     let nameProperty;
+    let priceProperty;
+    let filteredRestaurants;
+    let typeProperty;
+    let arrType;
 
     //city is always provided
     const cityProperty = { city: { [Op.like]: `%${req.query.city}%` } };
@@ -31,17 +34,39 @@ router.get('/?', async (req, res) => {
       nameProperty = { name: { [Op.like]: `'%${req.query.name}%'` } };
     }
 
+    //if price_level is provided by the user
+    if (req.query.price_level) {
+      //the user can provide more than one input
+      const arrPrice = req.query.price_level.split(',').map((priceItem) => {
+        return {
+          price_level: { [Op.like]: `%${priceItem}%` },
+        };
+      });
+      priceProperty = { [Op.or]: arrPrice };
+    }
+
+    //if type of restaurant is provided
+    if (req.query.type) {
+      //options are sit_down, quick_bites, cafe, etc.
+      arrType = req.query.type.split(',').map((type) => {
+        return {
+          sub_categories: { [Op.like]: `%${type}%` },
+        };
+      });
+      arrType.push({ sub_categories: { [Op.like]: `%%` } }); //add it if restaurants sub_categories is empty
+      console.log(arrType);
+      typeProperty = { [Op.or]: arrType };
+    }
+
     //search with the filters provided by the user
     filteredRestaurants = await Restaurant.findAll({
       where: {
-        // cuisine: { [Op.like]: '%sushi%' },
         [Op.and]: [
           cityProperty,
           nameProperty,
           cuisineProperty,
-          // { price_level: req.query.price_range || '' },
-          // { city: req.query.city || '' },
-          // { sub_categories: req.query.type || '' },
+          priceProperty,
+          typeProperty,
         ],
       },
     });
